1becbfe90641235d6177e2f5808d25fa4a65b937
Abe Augustijn <abe.augustijn@hotmail.nl>
2020-03-02 22:07:01 +0100

    Fix: had a leak because the xdisplay was not closed

--- X11/mlx_get_screen_size.c
+++ X11/mlx_get_screen_size.c
@@ -6,7 +6,7 @@
 /*   By: abe <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 19:45:38 by abe               #+#    #+#             */
-/*   Updated: 2020/02/28 19:51:11 by abe              ###   ########.fr       */
+/*   Updated: 2020/03/02 22:06:49 by abe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	mlx_get_screen_size(t_xvar *xvar, int *sizex, int *sizey)
 	s = DefaultScreenOfDisplay(d);
 	*sizex = s->width;
 	*sizey = s->height;
+	XCloseDisplay(d);
 	return (0);
 }
